6c726b8bcfc86a08dc218885a6ebc5c619e23473
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-19 19:20:51 +0800

    add quickling mode

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ var BigPipe = require('./lib/bigpipe.js');
 module.exports = function( options ) {
 
     return function(req, res, next) {
-        res.bigpipe = new BigPipe(options);
+        var bigpipe = res.bigpipe = new BigPipe(options);
+        var pagelet = req.param('pagelet');
+        var pagelets = req.param('pagelets');
+
+        if (!Array.isArray(pagelets)) {
+            pagelets = pagelets ? [ pagelets ] : [];
+        }
+
+        pagelet && pagelets.push(pagelet);
+        bigpipe.addQuicklingPagelets(pagelets);
 
         next();
     }
