805fc56f4cefc5cff580a12743cc9a4f826af2e4
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-24 14:41:37 +0800

    update bigpipe

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ module.exports = function( options ) {
         var bigpipe = res.bigpipe = new BigPipe(options);
         var pagelet = req.param('pagelet');
         var pagelets = req.param('pagelets');
+        var destroy = function() {
+            res.removeListener('finish', destroy);
+            res.removeListener('close', destroy);
+
+            bigpipe.destroy();
+            bigpipe = res.bigpipe = null;
+        };
 
         if (!Array.isArray(pagelets)) {
             pagelets = pagelets ? [ pagelets ] : [];
@@ -13,6 +20,9 @@ module.exports = function( options ) {
 
         pagelet && pagelets.push(pagelet);
         bigpipe.addQuicklingPagelets(pagelets);
+
+        res.on('finish', destroy);
+        res.on('close', destroy);
 
         next();
     }
